6d4770a98f1a0e3ab99960c6a8677761fb514c3f
tijntje <kasteleman>
2016-07-31 19:25:19 +0200

    Settings ZMNHDA2 completed.

--- README.md
+++ README.md
@@ -5,3 +5,7 @@ This app adds support following Qubino devices in Homey:
 ZMNHVD1 Flush Dimmer 0-10V
 ZMNHDA2 Flush Dimmer
 
+Changelog:
+
+31-07-2016 19.:15 Settings completed for ZMNHDA2 Flush Dimmer.Stil some errors in code. Have to check!
+
--- drivers/ZMNHDA2/driver.js
+++ drivers/ZMNHDA2/driver.js
@@ -4,6 +4,7 @@ const path			= require('path');
 const ZwaveDriver	= require('homey-zwavedriver');
 
 module.exports = new ZwaveDriver( path.basename(__dirname), {
+	debug: true,
 	capabilities: {
 
 				'onoff': {
@@ -44,9 +45,80 @@ module.exports = new ZwaveDriver( path.basename(__dirname), {
 					},
 				},
 			},
+
 		settings: {
 			"Input_1_type": {
 			"index": 1,
+			"size": 1,
+			"parser": function( input ) {
+				return new Buffer([ parseInt(input) ]);
+				}
+			},
+			"Input_2_contact_type": {
+			"index": 2,
+			"size": 1,
+			"parser": function( input ) {
+				return new Buffer([ parseInt(input) ]);
+				}
+			},
+			"Input_3_contact_type": {
+			"index": 3,
+			"size": 1,
+			"parser": function( input ) {
+				return new Buffer([ parseInt(input) ]);
+				}
+			},
+			"Deactivate/Activate_ALL_ON_/_ALL_OFF": {
+			"index": 10,
+			"size": 1,
+			"parser": function( input ) {
+				return new Buffer([ parseInt(input) ]);
+				}
+			},
+			"State_of_device_after_power_failure": {
+			"index": 30,
+			"size": 1,
+			"parser": function( input ) {
+				return new Buffer([ ( input === true ) ? 1 : 0 ]);
+				}
+			},
+			"Power_report_on_power_change": {
+			"index": 40,
+			"size": 1,
+			"parser": function( input ) {
+				return new Buffer([ parseInt(input) ]);
+				}
+			},
+			"Power_report_by_time_interval": {
+			"index": 42,
+			"size": 2,
+			"parser": function( input ) {
+				return new Buffer([ parseInt(input) ]);
+				}
+			},
+			"Maximum_dimming_value": {
+			"index": 61,
+			"size": 1,
+			"parser": function( input ) {
+				return new Buffer([ parseInt(input) ]);
+				}
+			},
+			"Minimum_dimming_value": {
+			"index": 60,
+			"size": 1,
+			"parser": function( input ) {
+				return new Buffer([ parseInt(input) ]);
+				}
+			},
+			"Dimming_time_(soft_on/off)": {
+			"index": 65,
+			"size": 1,
+			"parser": function( input ) {
+				return new Buffer([ parseInt(input) ]);
+				}
+			},
+			"Dimming_time_when_key_pressed": {
+			"index": 66,
 			"size": 1,
 			"parser": function( input ) {
 				return new Buffer([ parseInt(input) ]);
